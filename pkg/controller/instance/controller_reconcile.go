6c06d9db3c2f9808497f416cc67d2758f3a1e73b
Amine <hilalyamine@gmail.com>
2024-12-15 05:21:43 -0800

    Move internal to pkg

--- pkg/controller/instance/controller_reconcile.go
+++ pkg/controller/instance/controller_reconcile.go
@@ -25,10 +25,10 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
 
-	"github.com/awslabs/kro/internal/controller/instance/delta"
-	"github.com/awslabs/kro/internal/metadata"
-	"github.com/awslabs/kro/internal/runtime"
+	"github.com/awslabs/kro/pkg/controller/instance/delta"
+	"github.com/awslabs/kro/pkg/metadata"
 	"github.com/awslabs/kro/pkg/requeue"
+	"github.com/awslabs/kro/pkg/runtime"
 )
 
 // instanceGraphReconciler is responsible for reconciling a single instance and
